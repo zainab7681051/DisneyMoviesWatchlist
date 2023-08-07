831754999f8637d1e34ad59b3837949edbc74e7e
zainab7681051 <zainab76810510@gmail.com>
2023-08-08 01:59:13 +0300

    commit

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -15,11 +15,13 @@ namespace DisneyMoviesWatchlist.Areas.Identity.Pages.Account
 {
     public class LoginModel : PageModel
     {
+        private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
 
-        public LoginModel(SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger)
+        public LoginModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, ILogger<LoginModel> logger)
         {
+            _userManager=userManager;
             _signInManager = signInManager;
             _logger = logger;
         }
@@ -64,7 +66,8 @@ namespace DisneyMoviesWatchlist.Areas.Identity.Pages.Account
 
             if (ModelState.IsValid)
             {
-                var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var user= await _userManager.FindByEmailAsync(Input.Email);
+                var result = await _signInManager.PasswordSignInAsync(user.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
@@ -77,11 +80,8 @@ namespace DisneyMoviesWatchlist.Areas.Identity.Pages.Account
                 }
                 else
                 {
-                    _logger.LogWarning(typeof(result));
                     ModelState.AddModelError(string.Empty, "Invalid login attempt.");                
                     return Page();
-
-                    
                 }
             }
             return Page();
