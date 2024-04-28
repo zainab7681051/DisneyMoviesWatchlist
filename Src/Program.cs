a56ae8f23f30d931577ec20bb519a5b680ae4371
zainab 76810510 <zainab76810510@gmail.com>
2024-04-28 12:04:26 +0300

    commit

--- Src/Program.cs
+++ Src/Program.cs
@@ -5,7 +5,6 @@ using DisneyMoviesWatchlist.Src.Repository;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddDbContext<DisneyMoviesDbContext>();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(
