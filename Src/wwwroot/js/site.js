8e15ac071b824772a5b27a76567dc7f0738c8e26
zainab7681051 <zainab76810510@gmail.com>
2024-07-20 22:58:41 +0300

    new commit

--- Src/wwwroot/js/site.js
+++ Src/wwwroot/js/site.js
@@ -1,15 +1,1 @@
-﻿const app = document.getElementById('app')
-const img = document.querySelectorAll('img');
-const loading_screen = document.querySelector('.loading-screen-wrapper')
-let img_count = 0;
-for (let i = 0; i < img.length; i++) {
-    console.log('loading image', img_count);
-    img[i].onload = () => {
-        img_count++;
-        console.log('loaded', img_count);
-        if (img_count >= 2) {
-            loading_screen.style.display = "none";
-            app.removeAttribute('style');
-        }
-    }
-}
+﻿
